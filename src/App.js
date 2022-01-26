a161466f665214ac52028ad546f06c2417628280
wilson-munoz <69641501+wilson-munoz@users.noreply.github.com>
2022-01-26 00:35:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import NavBar from './components/NavBar/NavBar.js';
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <h2>Ofertas de la semana</h2>
+    </>
+  )
+}
+
+export default App;
